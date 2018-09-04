c206c26c6f75594dbad0bc4948a38de39930f0d3
bibek <bibeksinghthapa@gmail.com>
2018-09-04 15:51:47 -0400

    created router for home.js

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <h1>Dentist Website</h1>
+       <p>
+         Welcome to my dentist website.
+       </p>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
